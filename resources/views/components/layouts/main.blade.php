bc38f99684d3ec4358a2c1d8bc857aceaa543756
KarlisEmbergs <karlisembergs@gmail.com>
2026-01-12 19:08:54 +0200

    Global Styling Setup.

--- resources/views/components/layouts/main.blade.php
+++ resources/views/components/layouts/main.blade.php
@@ -1,12 +1,15 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     @include('partials.head')
 </head>
-<body>
-<flux:main>
-    {{$slot}}
-</flux:main>
-@fluxScripts
+
+<body class="container mx-auto">
+    <flux:main>
+        {{$slot}}
+    </flux:main>
+    @fluxScripts
 </body>
+
 </html>
