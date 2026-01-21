a34b0fa4c1ce7044d6e0c82d17ca617294563f25
KarlisEmbergs <karlisembergs@gmail.com>
2026-01-21 22:04:06 +0200

    Styling update for accordion.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,17 @@
 <x-layouts.main :title="__('Sākums')">
+
+    {{-- NAVIGATION --}}
     <x-nav />
+
+    {{-- HEADER --}}
     <x-header />
-    <x-services-price-table />
+
+    {{-- ACCORDION WRAPPER --}}
+    <div class="mx-auto max-w-7xl px-4 pb-12">
+        <flux:heading level="2">Pakalpojumi un cenas</flux:heading>
+        <flux:accordion transition>
+            <x-services-price-table />
+        </flux:accordion>
+    </div>
+
 </x-layouts.main>
