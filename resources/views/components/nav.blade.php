eb59969e25d06f8ab8cffec5a3aa9e8ce2185ca8
KarlisEmbergs <karlisembergs@gmail.com>
2026-01-21 18:43:40 +0200

    Add services and prices table.

---
+++ resources/views/components/nav.blade.php
@@ -0,0 +1,19 @@
+{{--
+<flux:sidebar collapsible dir="rtl" class="bg-zinc-900 w-full md:w-lg data-flux-sidebar-collapsed-desktop:hidden">
+    <flux:sidebar.toggle icon="x-mark" class="cursor-pointer" />
+    <flux:sidebar.nav class="text-center items-center">
+        <flux:sidebar.item href="#">Home</flux:sidebar.item>
+        <flux:sidebar.item href="#">Inbox</flux:sidebar.item>
+        <flux:sidebar.item href="#">Documents</flux:sidebar.item>
+        <flux:sidebar.item href="#">Calendar</flux:sidebar.item>
+        <flux:sidebar.item href="#">Calendar</flux:sidebar.item>
+    </flux:sidebar.nav>
+</flux:sidebar>
+
+<flux:header class="bg-zinc-900 flex justify-end">
+    <flux:sidebar.toggle icon="bars-3" inset="left" class="cursor-pointer" />
+</flux:header> --}}
+
+{{-- <flux:main> --}}
+    {{--
+</flux:main> --}}
---
+++ resources/views/components/services-price-table.blade.php
@@ -0,0 +1,56 @@
+<div class="mx-auto max-w-7xl px-4 pb-12">
+    <flux:heading level="2">Pakalpojumi un cenas</flux:heading>
+    <flux:accordion transition>
+        <flux:accordion.item>
+            <flux:accordion.heading>
+                <flux:heading level="3">Veselības vingrošana</flux:heading>
+            </flux:accordion.heading>
+            <flux:accordion.content>
+
+            </flux:accordion.content>
+        </flux:accordion.item>
+        <flux:accordion.item>
+            <flux:accordion.heading>
+                <flux:heading level="3">Funkcionālais treniņš</flux:heading>
+            </flux:accordion.heading>
+            <flux:accordion.content>
+                <flux:table>
+                    <flux:table.rows>
+                        <flux:table.row>
+                            <flux:table.cell>Grupu treniņš | 60min</flux:table.cell>
+                            <flux:table.cell align="center">35 EUR</flux:table.cell>
+                        </flux:table.row>
+                        <flux:table.row>
+                            <flux:table.cell>Individuāls treniņš | 60min</flux:table.cell>
+                            <flux:table.cell align="center">50 EUR</flux:table.cell>
+                        </flux:table.row>
+                    </flux:table.rows>
+                </flux:table>
+            </flux:accordion.content>
+        </flux:accordion.item>
+        <flux:accordion.item>
+            <flux:accordion.heading>
+                <flux:heading level="3">Joga</flux:heading>
+            </flux:accordion.heading>
+            <flux:accordion.content>
+
+            </flux:accordion.content>
+        </flux:accordion.item>
+        <flux:accordion.item>
+            <flux:accordion.heading>
+                <flux:heading level="3">VAV bērniem</flux:heading>
+            </flux:accordion.heading>
+            <flux:accordion.content>
+
+            </flux:accordion.content>
+        </flux:accordion.item>
+        <flux:accordion.item>
+            <flux:accordion.heading>
+                <flux:heading level="3">Bērnu fizioterapija</flux:heading>
+            </flux:accordion.heading>
+            <flux:accordion.content>
+
+            </flux:accordion.content>
+        </flux:accordion.item>
+    </flux:accordion>
+</div>
