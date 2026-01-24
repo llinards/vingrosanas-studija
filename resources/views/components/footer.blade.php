7d77a42e2daec165cda2e02ac34cd2ca3e21035c
KarlisEmbergs <karlisembergs@gmail.com>
2026-01-24 13:18:38 +0200

    Some styling fixes.

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,4 +1,4 @@
-<footer>
+<footer class="bg-beige">
     <div class="container mx-auto px-4">
         <div
             class="flex flex-col space-y-2 md:space-y-0 md:flex-row text-center md:text-right items-center justify-between border-y border-black py-3">
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,11 +7,13 @@
     <x-header />
 
     {{-- ACCORDION WRAPPER --}}
-    <div class="mx-auto max-w-7xl px-4 pb-12">
-        <flux:heading level="2" class="py-12">Pakalpojumi un cenas</flux:heading>
-        <flux:accordion transition>
-            <x-services-price-table />
-        </flux:accordion>
+    <div class="bg-beige">
+        <div class="mx-auto max-w-7xl px-4 pb-12">
+            <flux:heading level="2" class="py-12">Pakalpojumi un cenas</flux:heading>
+            <flux:accordion transition>
+                <x-services-price-table />
+            </flux:accordion>
+        </div>
     </div>
 
     {{-- FOOTER --}}
