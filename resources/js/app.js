e241b656d89ce6d39012f1faa70f197e90845631
KarlisEmbergs <karlisembergs@gmail.com>
2026-01-29 22:42:46 +0200

    Tweak gallery carousel.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,13 +1,20 @@
 import {Carousel} from '@fancyapps/ui';
 import {Dots} from '@fancyapps/ui/dist/carousel/carousel.dots.js';
+import {Autoplay} from '@fancyapps/ui/dist/carousel/carousel.autoplay.js';
 
 const galleryCarousel = document.getElementById('galleryCarousel');
 const ownerCarousel = document.getElementById('ownerCarousel');
 
 if (galleryCarousel) {
     Carousel(galleryCarousel, {
-        // Your custom options
+        Autoplay: {
+            pauseOnHover: false,
+            showProgressbar: false,
+            timeout: 3000,
+        },
+        gestures: false,
     }, {
+        Autoplay,
         Dots,
     }).init();
 }
@@ -16,7 +23,6 @@ if (ownerCarousel) {
     Carousel(ownerCarousel, {
         // Your custom options
     }, {
-        Autoplay: false,
         Dots,
     }).init();
 }
