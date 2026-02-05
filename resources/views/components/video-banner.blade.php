5246868d24cb81ed8797b2a81a1ee13b34a3a0c8
KarlisEmbergs <karlisembergs@gmail.com>
2026-02-05 20:42:49 +0200

    Add video banner.

--- resources/views/components/video-banner.blade.php
+++ resources/views/components/video-banner.blade.php
@@ -1,14 +1,14 @@
 <div id="videoBanner" class="full-width-banner relative pb-12" x-data="{ playing: false }">
     {{-- Thumbnail --}}
-    <div x-show="!playing" @click="playing = true" class="cursor-pointer relative h-full w-full">
+    <div x-show="!playing" @click="playing = true; $refs.video.play()" class="cursor-pointer relative h-full w-full">
         <img class="h-full w-full object-cover" src="{{ asset('images/anete_platkevica_9.jpg') }}" alt="">
         <flux:icon.play-btn class="centered-icon" />
     </div>
 
     {{-- Video --}}
-    <video x-show="playing" x-cloak class="w-full h-full object-cover" controls autoplay x-ref="video"
-        @click.away="playing = false; $refs.video.pause()">
-        <source src="{{ asset('videos/your-video.mp4') }}" type="video/mp4">
+    <video x-show="playing" x-cloak class="w-full h-full object-cover" controls x-ref="video"
+        @click.away="playing = false; $refs.video.pause(); $refs.video.currentTime = 0">
+        <source src="{{ asset('videos/rick-astley-never-gonna-give-you-up.mp4') }}" type="video/mp4">
         {{ __('Diemžēl video nav iespējams atskaņot') }}
     </video>
 </div>
