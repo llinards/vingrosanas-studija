54158dc66944a34fba3c86ec28934b09f0a845f5
KarlisEmbergs <karlisembergs@gmail.com>
2026-02-05 20:49:11 +0200

    FIX - Image alignment adjustment for image-quote section.

--- resources/views/components/image-quote.blade.php
+++ resources/views/components/image-quote.blade.php
@@ -2,9 +2,9 @@
     <div class="h-124 md:h-206">
         <img class="h-full w-full object-cover" src="{{ asset('images/anete_platkevica_7.jpg') }}" alt="">
     </div>
-    <div class="h-124 md:h-206 relative flex justify-center items-center">
-        <img class="h-full w-full absolute blur-sm object-cover" src="{{ asset('images/anete_platkevica_8.jpg') }}"
-            alt="">
+    <div class="h-124 md:h-206 relative flex justify-center items-center overflow-hidden">
+        <img class="h-full w-full scale-135 absolute blur-sm object-cover object-[center_10%]"
+            src="{{ asset('images/anete_platkevica_8.jpg') }}" alt="">
         <div class="absolute inset-0 bg-black/30"></div>
         <div
             class="h-full relative flex flex-col justify-center items-center text-center space-y-6 md:space-y-12 px-4 sm:px-6 md:px-8 lg:px-12 xl:px-24">
