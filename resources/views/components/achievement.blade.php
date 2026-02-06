475d6e6483d59e253eca781d087748a39174f6fb
KarlisEmbergs <karlisembergs@gmail.com>
2026-02-06 17:28:18 +0200

    Achievement section. Styling updates.

---
+++ resources/views/components/achievement.blade.php
@@ -0,0 +1,30 @@
+{{-- SLOGAN AND NUMBER --}}
+<div class="container mx-auto px-4">
+    <div id="counter" class="mb-12 pb-6 md:pb-0 px-4 border-12 border-blue rounded-4xl text-center">
+        <flux:heading level="2" class="text-center">{{ __('Kustība sākas tieši šeit!') }}</flux:heading>
+        <div class="flex items-center justify-center">
+            <flux:link class="flex!" href="https://maps.app.goo.gl/UdGP64Acxz2RVPJe7" target="_blank">
+                <flux:icon.map-pin />
+                {{ __('Strēlnieku iela 20 A, Sigulda') }}
+            </flux:link>
+        </div>
+        <div class="text-center flex flex-col md:flex-row items-center justify-evenly my-6 md:mb-12">
+            <div class="space-y-3">
+                <flux:text id="yearsOfExperience" class="counter-number">6</flux:text>
+                <flux:text>{{ __('Gadu pieredze') }}</flux:text>
+            </div>
+            <div class="space-y-3">
+                <flux:text id="totalCalories" class="counter-number">120k</flux:text>
+                <flux:text>{{ __('Patērētās kalorijas') }}</flux:text>
+            </div>
+            <div class="space-y-3">
+                <flux:text id="trainingCoaches" class="counter-number">3</flux:text>
+                <flux:text>{{ __('Profesionāli treneri') }}</flux:text>
+            </div>
+            <div class="space-y-3">
+                <flux:text id="totalClients" class="counter-number">100</flux:text>
+                <flux:text>{{ __('Apmierināti klienti') }}</flux:text>
+            </div>
+        </div>
+    </div>
+</div>
--- resources/views/components/⚡services-price-table.blade.php
+++ resources/views/components/⚡services-price-table.blade.php
@@ -18,24 +18,26 @@ new class extends Component {
 
 <div>
     @forelse ($this->serviceTypes as $serviceType)
-        <flux:accordion.item>
-            <flux:accordion.heading>
-                <flux:heading level="3">{{ $serviceType->name }}</flux:heading>
-            </flux:accordion.heading>
-            <flux:accordion.content>
-                <flux:table>
-                    <flux:table.rows>
-                        @foreach ($serviceType->services as $service)
-                            <flux:table.row>
-                                <flux:table.cell>{{ $service->name }}</flux:table.cell>
-                                <flux:table.cell>{{ Number::currency($service->price / 100, 'EUR') }}</flux:table.cell>
-                            </flux:table.row>
-                        @endforeach
-                    </flux:table.rows>
-                </flux:table>
-            </flux:accordion.content>
-        </flux:accordion.item>
+    <flux:accordion.item>
+        <flux:accordion.heading>
+            <flux:heading level="3">{{ $serviceType->name }}</flux:heading>
+        </flux:accordion.heading>
+
+        <flux:accordion.content>
+            <flux:table class="w-full table-fixed">
+                <flux:table.rows>
+                    @foreach ($serviceType->services as $service)
+                    <flux:table.row>
+                        <flux:table.cell class="whitespace-normal">{{ $service->name }}</flux:table.cell>
+                        <flux:table.cell class="w-28" align="end">{{ Number::currency($service->price / 100, 'EUR') }}
+                        </flux:table.cell>
+                    </flux:table.row>
+                    @endforeach
+                </flux:table.rows>
+            </flux:table>
+        </flux:accordion.content>
+    </flux:accordion.item>
     @empty
-        <flux:text>{{ __('Pakalpojumu informācija nav pieejama.') }}</flux:text>
+    <flux:text>{{ __('Pakalpojumu informācija nav pieejama.') }}</flux:text>
     @endforelse
 </div>
