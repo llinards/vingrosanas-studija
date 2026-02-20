abf27e61b84f73921c225f51a809d23025ee1adf
KarlisEmbergs <karlisembergs@gmail.com>
2026-02-20 13:08:41 +0200

    Small slogan fix.

--- resources/views/components/slogan.blade.php
+++ resources/views/components/slogan.blade.php
@@ -1,7 +1,7 @@
 {{-- SLOGAN AND NUMBER --}}
 <div id="slogan" class="container mx-auto px-4 py-12">
     <div class="py-12 px-4 border-8 md:border-12 border-blue rounded-4xl text-center">
-        <flux:heading level="2" class="text-center">{{ __('Kustība sākas tieši šeit!') }}</flux:heading>
+        <flux:heading level="2" class="px-8 md:px-0 text-center">{{ __('Kustība sākas tieši šeit!') }}</flux:heading>
         <div class="flex items-center justify-center">
             <flux:link class="flex!" href="https://maps.app.goo.gl/UdGP64Acxz2RVPJe7" target="_blank">
                 <flux:icon.map-pin />
