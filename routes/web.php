b9c1dfad44e0cd3c4dd73d85ab91507ee1269662
Linards Lazdiņš <linards_lazdins@hotmail.com>
2026-03-09 20:54:00 +0200

    Remove `mcamara/laravel-localization` package and associated configurations
    - Delete `laravellocalization.php` configuration file.
    - Remove `mcamara/laravel-localization` from `composer.json` and `composer.lock`.
    - Update `app.php` to exclude unused middleware.
    - Refactor web routes to eliminate localization logic.

--- routes/web.php
+++ routes/web.php
@@ -9,68 +9,60 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use Laravel\Fortify\Features;
-use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
 
 Route::post('stripe/webhook', StripeWebhookController::class)->name('stripe.webhook');
 
-Route::group([
-    'prefix' => LaravelLocalization::setLocale(), 'middleware' => [
-        'localeSessionRedirect',
-        'localizationRedirect',
-    ],
-], static function () {
-    Route::get('/', static function () {
-        return view('welcome');
-    })->name('home');
-    Route::get('/privacy-policy', static function () {
-        return view('privacy-policy');
-    })->name('privacy-policy');
+Route::get('/', static function () {
+    return view('welcome');
+})->name('home');
+Route::get('/privacy-policy', static function () {
+    return view('privacy-policy');
+})->name('privacy-policy');
 
-    Route::get('booking/{booking}/success', static function (Booking $booking, Request $request) {
-        if ($request->filled('session_id')) {
-            if ($request->session_id !== $booking->stripe_checkout_session_id) {
-                abort(404);
-            }
-        } else {
-            if ( ! Auth::check() || Auth::user()->email !== $booking->email) {
-                abort(404);
-            }
+Route::get('booking/{booking}/success', static function (Booking $booking, Request $request) {
+    if ($request->filled('session_id')) {
+        if ($request->session_id !== $booking->stripe_checkout_session_id) {
+            abort(404);
         }
+    } else {
+        if (! Auth::check() || Auth::user()->email !== $booking->email) {
+            abort(404);
+        }
+    }
 
-        return view('booking.success', ['booking' => $booking]);
-    })->name('booking.success')->withTrashed();
+    return view('booking.success', ['booking' => $booking]);
+})->name('booking.success')->withTrashed();
 
-    Route::middleware(['auth'])->prefix('admin')->name('admin.')->group(function () {
-        Route::redirect('/', 'admin/bookings')->name('dashboard');
+Route::middleware(['auth'])->prefix('admin')->name('admin.')->group(function () {
+    Route::redirect('/', 'admin/bookings')->name('dashboard');
 
-        Route::view('coaches', 'dashboard.coach-list')->name('coaches.index');
-        Route::livewire('coaches/create', 'coach.coach-create')->name('coaches.create');
-        Route::livewire('coaches/{coach}/edit', 'coach.coach-edit')->name('coaches.edit');
+    Route::view('coaches', 'dashboard.coach-list')->name('coaches.index');
+    Route::livewire('coaches/create', 'coach.coach-create')->name('coaches.create');
+    Route::livewire('coaches/{coach}/edit', 'coach.coach-edit')->name('coaches.edit');
 
-        Route::view('services', 'dashboard.service-list')->name('services.index');
-        Route::livewire('services/create', 'service.service-create')->name('services.create');
-        Route::livewire('services/{service}/edit', 'service.service-edit')->name('services.edit');
+    Route::view('services', 'dashboard.service-list')->name('services.index');
+    Route::livewire('services/create', 'service.service-create')->name('services.create');
+    Route::livewire('services/{service}/edit', 'service.service-edit')->name('services.edit');
 
-        Route::view('schedules', 'dashboard.schedule-list')->name('schedules.index');
-        Route::livewire('schedules/create', 'schedule.schedule-create')->name('schedules.create');
-        Route::livewire('schedules/{schedule}/edit', 'schedule.schedule-edit')->name('schedules.edit');
+    Route::view('schedules', 'dashboard.schedule-list')->name('schedules.index');
+    Route::livewire('schedules/create', 'schedule.schedule-create')->name('schedules.create');
+    Route::livewire('schedules/{schedule}/edit', 'schedule.schedule-edit')->name('schedules.edit');
 
-        Route::view('bookings', 'dashboard.booking-list')->name('bookings.index');
-        Route::livewire('bookings/create', 'booking.booking-create')->name('bookings.create');
-        Route::livewire('bookings/{booking}/edit', 'booking.booking-edit')->name('bookings.edit');
+    Route::view('bookings', 'dashboard.booking-list')->name('bookings.index');
+    Route::livewire('bookings/create', 'booking.booking-create')->name('bookings.create');
+    Route::livewire('bookings/{booking}/edit', 'booking.booking-edit')->name('bookings.edit');
 
-        Route::redirect('settings', 'settings/profile');
-        Route::livewire('settings/profile', Profile::class)->name('settings.profile');
-        Route::livewire('settings/password', Password::class)->name('settings.password');
-        Route::livewire('settings/two-factor', TwoFactor::class)
-             ->middleware(
-                 when(
-                     Features::canManageTwoFactorAuthentication()
-                     && Features::optionEnabled(Features::twoFactorAuthentication(), 'confirmPassword'),
-                     ['password.confirm'],
-                     [],
-                 ),
-             )
-             ->name('settings.two-factor');
-    });
+    Route::redirect('settings', 'settings/profile');
+    Route::livewire('settings/profile', Profile::class)->name('settings.profile');
+    Route::livewire('settings/password', Password::class)->name('settings.password');
+    Route::livewire('settings/two-factor', TwoFactor::class)
+        ->middleware(
+            when(
+                Features::canManageTwoFactorAuthentication()
+                && Features::optionEnabled(Features::twoFactorAuthentication(), 'confirmPassword'),
+                ['password.confirm'],
+                [],
+            ),
+        )
+        ->name('settings.two-factor');
 });
